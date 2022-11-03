dd85f649e298bdb507b4d9fa93957771b3e2f057
MinaSameh1 <minasameh1@gmail.com>
2022-11-03 09:45:56 +0200

    fixed lint issue.

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: 'Udagram',
   apiHost: 'http://udagram-api-dev.us-east-1.elasticbeanstalk.com',
-}
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
